86b145cf4c5c949ef169846bd5dbc65eecf75a95
qiuzhifei <qiuzhifei521@gmail.com>
2021-08-18 11:21:06 +0800

    feat: update apple login

--- BlueIntent/Classes/AppleLogin/BlueIntentAppleLogin.swift
+++ BlueIntent/Classes/AppleLogin/BlueIntentAppleLogin.swift
@@ -9,108 +9,121 @@
 import Foundation
 import AuthenticationServices
 
-extension BlueIntent {
-  class AppleLogin: NSObject {
-    enum LoginError {
-      // 系统不支持 Apple 登录
-      case systemNotSupported
-      // 不支持该授权方式
-      case credentialNotSupported
-      // 授权失败
-      case authError(Error)
-    }
+public extension BlueIntent.AppleLogin {
+  enum LoginError {
+    // 系统不支持 Apple 登录
+    case systemNotSupported
+    // 不支持该授权方式
+    case credentialNotSupported
+    // 授权失败
+    case authError(Error)
+  }
+  
+  enum LoginResult {
+    case success(_ oauth: OAuth)
+    case failure(_ error: LoginError)
+  }
+}
+
+public extension BlueIntent.AppleLogin {
+  struct OAuth {
+    public let user: String
+    public let authorizationCode: String
+    public let fullName: PersonNameComponents?
+    public let email: String?
     
-    enum LoginResult {
-      case success(_ oauth: OAuth)
-      case failure(_ error: LoginError)
-    }
-    
-    struct OAuth {
-      let user: String
-      let authorizationCode: String
-      var fullName: PersonNameComponents?
-      var email: String?
-      
-      @available(iOS 13.0, *)
-      init?(_ credential: ASAuthorizationAppleIDCredential?) {
-        guard let credential = credential else { return nil }
-        if let code = credential.authorizationCode,
-           let authorizationCode = String(data: code, encoding: .utf8) {
-          self.authorizationCode = authorizationCode
-        } else {
-          return nil
-        }
-        
-        self.user = credential.user
-        self.fullName = credential.fullName
-        self.email = credential.email
-      }
-    }
-    
-    static let shared = AppleLogin()
-    
-    fileprivate var result: ((LoginResult) -> ())?
-    
-    static func isAppleLoginSupport() -> Bool {
-      if #available(iOS 13.0, *) {
-        return true
-      }
-      return false
-    }
-    
-    func login(_ result: ((LoginResult) -> ())?) {
-      self.result = result
-      
-      if #available(iOS 13.0, *) {
-        let appleIDProvider = ASAuthorizationAppleIDProvider()
-        let request = appleIDProvider.createRequest()
-        request.requestedScopes = [.fullName, .email]
-        
-        let authorizationController =
-          ASAuthorizationController(authorizationRequests: [request])
-        authorizationController.delegate = self
-        authorizationController.presentationContextProvider = self
-        authorizationController.performRequests()
-        return
+    @available(iOS 13.0, *)
+    init?(_ credential: ASAuthorizationAppleIDCredential?) {
+      guard let credential = credential else { return nil }
+      if let code = credential.authorizationCode,
+         let authorizationCode = String(data: code, encoding: .utf8) {
+        self.authorizationCode = authorizationCode
+      } else {
+        return nil
       }
       
-      result?(.failure(.systemNotSupported))
+      self.user = credential.user
+      self.fullName = credential.fullName
+      self.email = credential.email
     }
   }
 }
 
+extension BlueIntent {
+  public class AppleLogin: NSObject {
+    private static let shared = AppleLogin()
+    
+    private var result: ((LoginResult) -> ())?
+  }
+}
+
+public extension BlueIntent.AppleLogin {
+  static func systemSupported() -> Bool {
+    if #available(iOS 13.0, *) {
+      return true
+    }
+    return false
+  }
+  
+  static func login(_ result: ((LoginResult) -> ())?) {
+    Self.shared.login(result)
+  }
+}
+
+private extension BlueIntent.AppleLogin {
+  func login(_ result: ((LoginResult) -> ())?) {
+    self.result = result
+    
+    if #available(iOS 13.0, *) {
+      let appleIDProvider = ASAuthorizationAppleIDProvider()
+      let request = appleIDProvider.createRequest()
+      request.requestedScopes = [.fullName, .email]
+      
+      let authorizationController =
+        ASAuthorizationController(authorizationRequests: [request])
+      authorizationController.delegate = self
+      authorizationController.presentationContextProvider = self
+      authorizationController.performRequests()
+      return
+    }
+    
+    result?(.failure(.systemNotSupported))
+  }
+}
+
+@available(iOS 13.0, *)
 extension BlueIntent.AppleLogin: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
-  @available(iOS 13.0, *)
-  func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-    switch authorization.credential {
-    case let appleIDCredential as ASAuthorizationAppleIDCredential:
+  public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+    let credential = authorization.credential
+    if let appleIDCredential = credential as? ASAuthorizationAppleIDCredential {
       if let oauth = OAuth(appleIDCredential) {
         self.result?(.success(oauth))
         return
       }
-      self.result?(.failure(.credentialNotSupported))
-    case _ as ASPasswordCredential:
-      self.result?(.failure(.credentialNotSupported))
-    default:
-      self.result?(.failure(.credentialNotSupported))
-      break
+      let error = ASAuthorizationError(.unknown)
+      self.result?(.failure(LoginError.authError(error)))
+      return
     }
+    if credential is ASPasswordCredential {
+      self.result?(.failure(.credentialNotSupported))
+      return
+    }
+    let error = ASAuthorizationError(.unknown)
+    self.result?(.failure(LoginError.authError(error)))
   }
   
-  @available(iOS 13.0, *)
-  func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+  public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
     self.result?(.failure(BlueIntent.AppleLogin.LoginError.authError(error)))
   }
   
-  @available(iOS 13.0, *)
-  func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+  public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
     return UIApplication.shared.windows.first!
   }
 }
 
-extension BlueIntent.AppleLogin.LoginError {
+public extension BlueIntent.AppleLogin.LoginError {
   // 中文错误
-  func cn() -> String {
+  var cn: String {
     switch self {
     case .credentialNotSupported:
       return "授权方式暂不支持"
