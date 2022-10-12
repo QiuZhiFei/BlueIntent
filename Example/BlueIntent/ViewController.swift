28d92b967c19b35bd1f511d4f4ec9fb3221ed804
qiuzhifei <qiuzhifei521@gmail.com>
2022-10-12 17:31:28 +0800

    f

--- Example/BlueIntent/ViewController.swift
+++ Example/BlueIntent/ViewController.swift
@@ -17,29 +17,39 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
 
-    let image = UIImage(named: "IMG_7459")!
-    
-    debugPrint(image.getColors(quality: .low)?.background.bi.rgba)
-    
-    let color = image.bi.getDominantColor()
-    view.backgroundColor = color
-    debugPrint(color?.bi.rgba)
+//    let image = UIImage(named: "IMG_0666")!
+//
+//    debugPrint(image.getColors(quality: .low)?.background.bi.rgba)
+//
+//    let color = image.bi.getDominantColor1(pixelLimit: 30000)
+//    view.backgroundColor = color
+//    debugPrint(color?.bi.rgba)
+
+
     
 //    debugPrint(image.dominantColors().first?.bi.rgba)
     
 //    view.backgroundColor = image.getColors()?.background
     
     
-//    let whiteImage = UIImage(named: "1")!
+    let whiteImage = UIImage(named: "1")!
 //    getTime(string: "0") {
 //      debugPrint(whiteImage.bi.getDominantColor()?.bi.hexString)
 //    }
 //
-//    getTime(string: "1") {
-//      debugPrint(whiteImage.bi.getDominantColor1()?.bi.hexString)
-//    }
+    getTime(string: "1") {
+      debugPrint(whiteImage.bi.getDominantColor1()?.bi.hexString)
+    }
   }
-  
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    let image = UIImage(named: "IMG_7459")!
+    getTime(string: "0") {
+      let color = image.bi.getDominantColor(pixelLimit: 6000)
+//          debugPrint()
+    }
+  }
 }
 
 func getTime(string: String, method: (() -> ())?) {
